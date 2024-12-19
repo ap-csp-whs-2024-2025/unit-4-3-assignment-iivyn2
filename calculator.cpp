4afb87105b822013f7ddafb527aab49046aa11eb
iivyn2 <idnguyen101@student.hbuhsd.edu>
2024-12-19 21:47:28 +0000

    finished yay

--- calculator.cpp
+++ calculator.cpp
@@ -15,7 +15,7 @@ int main()
   int denominator;
   while (user_input != 99)
   {
-    std::cout << "Welcome to C++ Calculator!  Choose an option below!\n"
+    std::cout << "Welcome to C++ Calculator! Choose an option below!\n"
               << "0. Roll a dice\n"        
               << "1. Add\n"
               << "2. Subtract\n"
@@ -27,7 +27,7 @@ int main()
     std::cin >> user_input;
     
     // implement calculator below
-    if (user_input == 0)
+      if (user_input == 0)
     {
       int random_number = (std::rand() % 6) + 1;  // random numbers between 1 and 6
       std::cout << "You rolled a " << random_number << std::endl;
@@ -40,32 +40,65 @@ int main()
         std::cin >> num1 >> num2;
         std::cout << "Their sum is " << (num1 + num2) << std::endl;
       }
-      else
-      { 
-        If (use_input == 2) 
-        {
-         std::cout << "Enter two numbers\n";
-        std::cin >> num1 >> num2;
-        std::cout << "Their difference is " << (num1 - num2) << std::endl;
-      }
       else 
-      {
-        If (user_input == 3)
         {
-           std::cout << "Enter two numbers\n";
-        std::cin >> num1 >> num2;
-        std::cout << "Their product is " << (num1 * num2) << std::endl;
+        if (user_input == 2)
+        {
+          std::cout << "Enter two numbers\n";
+          std::cin >> num1 >> num2;
+          std::cout << "Their difference is " << (num1 - num2) << std::endl;
         }
-      } 
-      else 
-      { 
-        If (user_input == 4)
-        { 
-           std::cout << "Enter two numbers\n";
-        std::cin >> numerator >> denominator;
-          {if (denominator == 0) 
-          { }}
-        }
-      }
+        else
+        {
+          if (user_input == 3)
+          {
+            std::cout << "Enter two numbers\n";
+            std::cin >> num1 >> num2;
+            std::cout << "Their product is " << (num1 * num2) << std::endl;
+          }
+          else
+          { 
+            if (user_input == 4)
+            {
+              std::cout << "Enter two numbers\n";
+              std::cin >> numerator >> denominator;
+              if (denominator == 0) 
+              {
+                std::cout << "Error: Division by zero is not allowed. \n";
+              }
+              else
+              {
+                std:: cout << "Their quotient is " << (numerator / denominator) << std::endl;
+              }
+            }
+            else
+            {
+              if (user_input == 5) 
+              { 
+                std::cout << "Enter two numbers\n";
+                std::cin >> num1;
+                if (num1 < 0)
+                {
+                  std::cout << "Error Cannot square root a negative number.\n";
+                }
+                else
+                {
+                  std::cout << "the square root of " << num1 << " is " << sqrt(num1) << std::endl;
+                }
+              }
+              else
+              {
+                if (user_input != 99)
+                {
+                  std::cout << "Invalid option, please choose again. \n";
+                } // end if invalid input
+              } // end of if else 5 
+            } // end of if else 4
+          } // end of if else 3
+        } // end of if else 2 
+        } // end of if else 1
+      } // end if else 0
+  }
+    std::cout << "Goodbye!\n"; 
   return 0;
 }
